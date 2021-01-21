6c2662be758c6123a6ca9ea81dc510116e9fa2d8
leeatwill <76966562+leeatwill@users.noreply.github.com>
2021-01-21 10:06:02 -0600

    Update build.sh

--- dev/build.sh
+++ dev/build.sh
@@ -31,16 +31,16 @@ fi
 popd
 
 # Build LoRa gateway app for this specific platform
-if [ ! -d lora_gateway ]; then
-    git clone -b legacy https://github.com/TheThingsNetwork/lora_gateway.git
-    pushd lora_gateway
+if [ ! -d single_chan_pkt_fwd ]; then
+    git clone https://github.com/adafruit/single_chan_pkt_fwd.git
+    pushd single_chan_pkt_fwd
 else
-    pushd lora_gateway
+    pushd single_chan_pkt_fwd
     git fetch origin
     git checkout legacy
     git reset --hard
 fi
-sed -i -e 's/PLATFORM= kerlink/PLATFORM= imst_rpi/g' ./libloragw/library.cfg
+# sed -i -e 's/PLATFORM= kerlink/PLATFORM= imst_rpi/g' ./libloragw/library.cfg
 # Comment the following in or out as needed for hardware debugging
 #sed -i -e 's/DEBUG_SPI= 0/DEBUG_SPI= 1/g' ./libloragw/library.cfg
 #sed -i -e 's/DEBUG_REG= 0/DEBUG_REG= 1/g' ./libloragw/library.cfg
@@ -51,23 +51,23 @@ make
 popd
 
 # Build the packet forwarder
-if [ ! -d packet_forwarder ]; then
-    git clone -b legacy https://github.com/TheThingsNetwork/packet_forwarder.git
-    pushd packet_forwarder
-else
-    pushd packet_forwarder
-    git fetch origin
-    git checkout legacy
-    git reset --hard
-fi
-make
-popd
+# if [ ! -d packet_forwarder ]; then
+#     git clone -b legacy https://github.com/TheThingsNetwork/packet_forwarder.git
+#     pushd packet_forwarder
+# else
+#     pushd packet_forwarder
+#     git fetch origin
+#     git checkout legacy
+#     git reset --hard
+# fi
+# make
+# popd
 
 # Restore location back to where we were prior to starting the build
 popd
 
 # Copy things needed at runtime to where they'll be expected
-cp $BUILD_DIR/packet_forwarder/reset_pkt_fwd.sh $INSTALL_DIR/set-gateway-id.sh
-cp $BUILD_DIR/packet_forwarder/poly_pkt_fwd/poly_pkt_fwd $INSTALL_DIR/ttn-gateway
+# cp $BUILD_DIR/packet_forwarder/reset_pkt_fwd.sh $INSTALL_DIR/set-gateway-id.sh
+# cp $BUILD_DIR/packet_forwarder/poly_pkt_fwd/poly_pkt_fwd $INSTALL_DIR/ttn-gateway
 
 echo "Build & Installation Completed."
